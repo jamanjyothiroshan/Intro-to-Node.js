c034b368d85247697c209ac8466fbc2d0325f94e
Roshan Jamanjyothi <112624255+jamanjyothiroshan@users.noreply.github.com>
2024-06-19 14:24:06 +0530

    Add files via upload

---
+++ June18 - Intro to Node.js/app.js
@@ -0,0 +1,41 @@
+console.log("ICC T20 World cup standings Groupwise");
+
+let groupA = [
+    "India",
+    "USA",
+    "Pakistan",
+    "Canada",
+    "Ireland"
+];
+
+console.log(groupA);
+
+let groupB = [
+    "Australia",
+    "England",
+    "Scotland",
+    "Namibia",
+    "Oman"
+];
+
+console.log(groupB);
+
+let groupC = [
+    "West Indies",
+    "Afganistan",
+    "New Zeland",
+    "Uganda",
+    "PNG"
+];
+
+console.log(groupC);
+
+let groupD = [
+    "South Africa",
+    "Bangladesh",
+    "Sri Lanka",
+    "Nedtherland",
+    "Nepal"
+];
+
+console.log(groupD);
